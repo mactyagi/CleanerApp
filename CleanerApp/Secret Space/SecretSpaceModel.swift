5e65dc8bed06ba11ef579e905b832828c7d26db3
Mactyagi <mactyagi@icloud.com>
2023-11-17 22:42:08 +0530

    working on Secret space Feature

---
+++ CleanerApp/Secret Space/SecretSpaceModel.swift
@@ -0,0 +1,8 @@
+//
+//  SecretSpaceModel.swift
+//  CleanerApp
+//
+//  Created by manu on 15/11/23.
+//
+
+import Foundation
---
+++ CleanerApp/Secret Space/SecretSpaceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecretSpaceViewController.swift
+//  CleanerApp
+//
+//  Created by manu on 15/11/23.
+//
+
+import UIKit
+
+class SecretSpaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
