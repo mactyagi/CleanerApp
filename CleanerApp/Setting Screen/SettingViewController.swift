df17fffaafae15d9904c818754cd336b211e5f37
Mac <mactyagi@icloud.com>
2024-05-15 12:01:47 +0530

    addedDescription, working on setting vc

---
+++ CleanerApp/Setting Screen/SettingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  Setting View Screen.swift
+//  CleanerApp
+//
+//  Created by Mac on 15/05/24.
+//
+
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
