06855baa001a805128eb0cc298da861c26deab2b
snehatyagi9810 <140044808+snehatyagi9810@users.noreply.github.com>
2024-10-13 12:50:58 +0530

    webview setup and  disable crashlytics  on debug mode

--- CleanerApp/Modules/Setting Screen/PrivacyPolicyViewController/PrivacyPolicyViewController.swift
+++ CleanerApp/Modules/Setting Screen/PrivacyPolicyViewController/PrivacyPolicyViewController.swift
@@ -7,49 +7,54 @@
 
 import UIKit
 import WebKit
+import SwiftUI
 
-class PrivacyPolicyViewController: UIViewController {
-    
-    //MARK: - IBOutlet
-    var webView: WKWebView!
-    
-        //MARK: - lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUpView()
+    class PrivacyPolicyViewController: UIViewController{
+        
+        
+        //MARK: - IBOutlet
+        var webView: WKWebView!
+        
+            //MARK: - lifecycle
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setUpView()
+            
+        }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            tabBarController?.tabBar.isHidden = true
+            super.viewWillAppear(animated)
+            navigationController?.navigationBar.prefersLargeTitles = false
+        }
+        
+        override func viewDidDisappear(_ animated: Bool) {
+            super.viewDidDisappear(animated)
+            navigationController?.navigationBar.isHidden = false
+            tabBarController?.tabBar.isHidden = false
+        }
+        
+        override func loadView() {
+            webView = WKWebView()
+            webView.navigationDelegate = self
+            view = webView
+            
+        }
+        
+        
+        //MARK: - setup fuctions
+        func setUpView(){
+            title = "Privacy Policy"
+            let url = URL(string: "https://www.termsfeed.com/live/7cf1c096-c229-4f85-a616-6c76e43fb351")!
+            webView.load(URLRequest(url: url))
+            webView.allowsBackForwardNavigationGestures = true
+        }
+  }
+
+
+    //MARK: - Web view delegate
+    extension PrivacyPolicyViewController : WKNavigationDelegate {
         
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        tabBarController?.tabBar.isHidden = true
-        super.viewWillAppear(animated)
-        navigationController?.navigationBar.prefersLargeTitles = false
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        navigationController?.navigationBar.isHidden = false
-        tabBarController?.tabBar.isHidden = false
-    }
-    
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
-    
-    
-    //MARK: - setup fuctions
-    func setUpView(){
-        title = "Privacy Policy"
-        let url = URL(string: "https://www.termsfeed.com/live/7cf1c096-c229-4f85-a616-6c76e43fb351")!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
-    }
-}
 
-
-//MARK: - Web view delegate
-extension PrivacyPolicyViewController : WKNavigationDelegate {
-    
-}
+//}
